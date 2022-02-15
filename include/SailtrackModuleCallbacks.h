d2a5c6383bb1fbfa489b493dfc78f2ba08b2683e
Matteo Carnelos <matteo.carnelos@studenti.unipd.it>
2022-02-15 15:23:12 +0100

    Fix callbacks calls (#8 #9)

--- include/SailtrackModuleCallbacks.h
+++ include/SailtrackModuleCallbacks.h
@@ -6,9 +6,12 @@ class SailtrackModuleCallbacks {
         virtual void onWifiConnectionBegin() {}
         virtual void onWifiConnectionResult(wl_status_t status) {}
         virtual void onWifiDisconnected() {}
-        virtual void onMqttConnected() {}
-        virtual void onMqttMessage(const char * topic, const char * message) {}
+
+        virtual void onMqttConnectionBegin() {}
+        virtual void onMqttConnectionResult(bool connected) {}
         virtual void onMqttDisconnected() {}
+        virtual void onMqttMessage(const char * topic, const char * message) {}
+
         virtual DynamicJsonDocument getStatus();
 };
 
